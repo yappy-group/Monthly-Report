5b792418fd3d85950e12da82975523bbd9bcbf1e
Jamie Egan <jamieegan@yappy.com.au>
2025-12-05 16:15:51 +0800

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/Monthly-Report/" : "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
